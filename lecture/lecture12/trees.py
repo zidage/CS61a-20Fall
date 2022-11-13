451e0a6532a2ce90da455aff43925dcd102175c4
zidage <1026017127@qq.com>
2022-11-13 19:45:02 +0800

    finished lecture12 tree on 13/11/2022

--- lecture/lecture12/trees.py
+++ lecture/lecture12/trees.py
@@ -39,3 +39,40 @@ def leaves(tree):
         return [lable(tree)]
     else:
         return sum([leaves(s) for s in branches(tree)], [])
+
+def increment_leaves(t):
+    """Return a tree like t but with leaf lables incremented"""
+    if is_leaf(t):
+        return tree(lable(t) + 1)
+    else:
+        bs = [increment_leaves(b) for b in branches(t)]
+        return tree(lable(t), bs)
+
+def increment(t):
+    """Return a tree like t but with all labels incremented"""
+    return tree(lable(t) + 1, [increment(b) for b in branches(t)])
+
+def print_tree(t, indent=0):
+    print('  ' * indent + str(lable(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+def fact_times(n, k):
+    """Return k * n * (n-1) * ... * 1"""
+    if n == 0:
+        return k
+    else:
+        return fact_times(n - 1, k * n)
+
+
+numbers = tree(3, [tree(4), tree(5, [tree(6)])])
+
+haste = tree('h', [tree('a', [tree('s'), tree('t')]), tree('e')])
+
+def print_sums(t, so_far):
+    so_far = so_far + lable(t)
+    if is_leaf(t):
+        print(so_far)
+    else:
+        for b in branches(t):
+            print_sums(b, so_far)
