f1a43d03ddc866bef684cb8efec95e4c2155788e
zidage <1026017127@qq.com>
2022-11-13 12:46:57 +0800

    trivial changes

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -166,4 +166,4 @@ def add_chars(w1, w2):
     if w1[0] == w2[0]:
         return add_chars(w1[1:], w2[1:])
     else:
-        return w2[0] + add_chars(w1, w2[1:])
+        return w2[0] + add_chars(w1, w2[1:]) #from PKUFlypig
